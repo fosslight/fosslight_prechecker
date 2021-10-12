ac0fd00b24c770c2f869411f73e0dd410c26c86f
Jaekwon Bang <jaekwon.bang@lge.com>
2021-10-12 16:10:50 +0900

    Modify to print help msg one time

--- src/fosslight_reuse/_help.py
+++ src/fosslight_reuse/_help.py
@@ -5,16 +5,16 @@
 from fosslight_util.help import PrintHelpMsg
 
 _HELP_MESSAGE_REUSE = """
-    Usage: fosslight_reuse [Mode] [option1] <arg1> [option2] <arg2>...
-        ex) fosslight_reuse lint -p /home/test/ -f "notice/sample.py,src/init.py"
-            fosslight_reuse add -p /home/test/ -c "Copyright 2019-2021 LG Electronics Inc." -l "GPL-3.0-only"
-
     FOSSLight Reuse is a Tool to check REUSE compliance in source code.
+
+    Usage: fosslight_reuse [Mode] [option1] <arg1> [option2] <arg2>...
+     ex) fosslight_reuse lint -p /home/test/ -f "notice/sample.py,src/init.py"
+         fosslight_reuse add -p /home/test/ -c "2019-2021 LG Electronics Inc." -l "GPL-3.0-only"
 
     Parameters:
         Mode
             lint\t\t    Check REUSE compliance
-            report\t\t    oss_pkg_info.yaml <-> FOSSLight-Report.xml
+            report\t\t    oss_pkg_info.yaml <-> FOSSLight-Report.xlsx
             add\t\t\t    Add missing license and copyright
 
         Options:
@@ -25,7 +25,7 @@ _HELP_MESSAGE_REUSE = """
             -n\t\t\t    Don't exclude venv*, node_modules, and .*/ from the analysis
         Options for only 'add' mode
             -l <license>\t    License name(SPDX format) to add
-            -c <copyright>\t    Copyright to add(ex, Copyright 2015-2021 LGE Electronics Inc.)"""
+            -c <copyright>\t    Copyright to add(ex, 2015-2021 LG Electronics Inc.)"""
 
 
 def print_help_msg(exitOpt=True):
--- src/fosslight_reuse/cli.py
+++ src/fosslight_reuse/cli.py
@@ -3,14 +3,14 @@
 # Copyright (c) 2021 LG Electronics Inc.
 # SPDX-License-Identifier: GPL-3.0-only
 import argparse
-from ._help import _HELP_MESSAGE_REUSE
+from ._help import print_help_msg
 from ._fosslight_reuse import run_lint
 from fosslight_oss_pkg.fosslight_oss_pkg import convert_report
 from ._add import add_content
 
 
 def main():
-    parser = argparse.ArgumentParser(description='FOSSLight Reuse', usage=_HELP_MESSAGE_REUSE)
+    parser = argparse.ArgumentParser(description='FOSSLight Reuse', prog='fosslight_reuse', add_help=False)
     parser.add_argument('mode', help='lint | report | add')
     parser.add_argument('--path', '-p', help='Path to check', type=str, dest='path', default="")
     parser.add_argument('--file', '-f', help='Files to check', type=str, dest='file', default="")
@@ -18,7 +18,15 @@ def main():
     parser.add_argument('--no', '-n', help='Disable automatic exclude mode', action='store_true', dest='disable')
     parser.add_argument('--license', '-l', help='License name to add', type=str, dest='license', default="")
     parser.add_argument('--copyright', '-c', help='Copyright to add', type=str, dest='copyright', default="")
-    args = parser.parse_args()
+    parser.add_argument('--help', '-h', help='Print help message', action='store_true', dest='help')
+    try:
+        args = parser.parse_args()
+    except:
+        print_help_msg()
+
+    if args.help:
+        print_help_msg()
+
 
     if args.mode == "lint":
         run_lint(args.path, args.file, args.disable, args.output)
