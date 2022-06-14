e59b326a2d5ed168c5a2412fd2ee528364f99cf6
Soim Kim <soim.kim@lge.com>
2022-06-14 11:42:20 +0900

    Separate exception handling files

---
+++ src/fosslight_reuse/_constant.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022 LG Electronics Inc.
+# SPDX-License-Identifier: GPL-3.0-only
+
+DEFAULT_EXCLUDE_EXTENSION = ["jar", "png", "exe", "so", "a", "dll", "jpeg", "jpg", "ttf", "lib", "ttc", "pfb",
+                             "pfm", "otf", "afm", "dfont", "json"]
+OSS_PKG_INFO_FILES = ["oss-pkg-info.yaml", "oss-pkg-info.yml", "oss-package.info", "requirement.txt",
+                      "requirements.txt", "package.json", "pom.xml", "build.gradle","podfile.lock", "cartfile.resolved"]
