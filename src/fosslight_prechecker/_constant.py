2e73ba79a8cf8f8d4aae48fbd0258943272d2d64
Jaekwon Bang <jaekwon.bang@lge.com>
2022-08-09 08:43:34 +0900

    Change sample yaml file name - sbom-info.yaml

--- src/fosslight_prechecker/_constant.py
+++ src/fosslight_prechecker/_constant.py
@@ -5,9 +5,9 @@
 
 DEFAULT_EXCLUDE_EXTENSION = ["jar", "png", "exe", "so", "a", "dll", "jpeg", "jpg", "ttf", "lib", "ttc", "pfb",
                              "pfm", "otf", "afm", "dfont", "json"]
-OSS_PKG_INFO_FILES = [r"oss-pkg-info[\s\S]*.yaml", "oss-pkg-info.yml", r"oss-package[\s\S]*.info", "requirement.txt",
-                      "requirements.txt", "package.json", "pom.xml", "build.gradle", "podfile.lock", "cartfile.resolved",
-                      "pubspec.yaml", "package.resolved", "go.mod", r"fosslight-sbom-info[\s\S]*.yaml", r"sbom-info[\s\S]*.yaml"]
+OSS_PKG_INFO_FILES = [r"oss-pkg-info[\s\S]*.ya?ml", r"oss-package[\s\S]*.info", r"sbom-info[\s\S]*.ya?ml",
+                      "requirement.txt", "requirements.txt", "package.json", "pom.xml", "build.gradle",
+                      "podfile.lock", "cartfile.resolved", "pubspec.yaml", "package.resolved", "go.mod"]
 HTML_RESULT_EXPAND_LIMIT = 10
 HTML_RESULT_PRINT_LIMIT = 100
 
