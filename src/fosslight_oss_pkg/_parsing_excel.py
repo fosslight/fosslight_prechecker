5e0b0ca5637b4ffdab56c095dd73030b4172606e
Jaekwon Bang <jaekwon.bang@lge.com>
2022-09-15 19:18:05 +0900

    Convert several .yaml file to a report file

--- src/fosslight_oss_pkg/_parsing_excel.py
+++ src/fosslight_oss_pkg/_parsing_excel.py
@@ -10,14 +10,38 @@ from fosslight_util.output_format import write_output_file
 
 logger = logging.getLogger(LOGGER_NAME)
 
+HEADER_CONTENT = ['ID', 'Source Name or Path', 'OSS Name',
+                  'OSS Version', 'License',  'Download Location',
+                  'Homepage', 'Copyright Text', 'Exclude',
+                  'Comment']
+MAX_SHEET_NAME_LEN = 31
+
+
+def get_sheet_name(yaml_file, sheet_list):
+    if len(yaml_file) > MAX_SHEET_NAME_LEN:
+        yaml_file = yaml_file[0:MAX_SHEET_NAME_LEN]
+
+    count = 1
+    while yaml_file in sheet_list:
+        end_idx = MAX_SHEET_NAME_LEN - 1 - len(str(count))
+        yaml_file = f"{yaml_file[0:end_idx]}_{count}"
+        count += 1
+        if end_idx == 0:
+            yaml_file = ""
+            logger.error("Too many duplicated file names")
+            break
+    return yaml_file
+
 
 def convert_yml_to_excel(oss_yaml_files, output_file, file_option_on, base_path):
-    items_to_print = []
     sheet_list = {}
+    header = {}
 
     for yaml_file in oss_yaml_files:
         try:
             if os.path.isfile(yaml_file):
+                items_to_print = []
+
                 logger.info(f"Read data from : {yaml_file}")
 
                 if file_option_on:
@@ -25,12 +49,20 @@ def convert_yml_to_excel(oss_yaml_files, output_file, file_option_on, base_path)
                 oss_items, _ = parsing_yml(yaml_file, base_path)
                 for item in oss_items:
                     items_to_print.extend(item.get_print_array())
+                if not base_path.endswith("/"):
+                    base_path += "/"
+                yaml_file = yaml_file.replace(base_path, '')
+                yaml_file = yaml_file.replace('/', '_')
+
+                yaml_file_sheet = get_sheet_name(yaml_file, sheet_list)
+                if yaml_file_sheet:
+                    sheet_list[yaml_file_sheet] = items_to_print
+                    header[yaml_file_sheet] = HEADER_CONTENT
         except Exception as ex:
             logger.error(f"Read yaml file: {ex}")
 
     try:
-        sheet_list["SRC_FL_Prechecker"] = items_to_print
-        success, msg, result_file = write_output_file(output_file, '.xlsx', sheet_list)
+        success, msg, result_file = write_output_file(output_file, '.xlsx', sheet_list, header)
         if success:
             if result_file:
                 logger.warning(f"Output: {result_file}")
